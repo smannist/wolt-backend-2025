24cf093940624eed27ec1398352129548aecf7bb
Sami Männistö <sami.mannisto1@gmail.com>
2025-01-27 12:12:51 +0200

    Add missing mock dependencies to tests

--- tests/integration/router_delivery_test.py
+++ tests/integration/router_delivery_test.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.mark.anyio
 async def test_endpoint_returns_correct_error_with_missing_query_user_lon_param(
-        test_client):
+        test_client, mock_delivery_route_dependencies):
     """Tests that the API returns status code 400 with an error indication that single parameter is missing"""
     response = test_client.get(
         "/api/v1/delivery-order-price?venue_slug=home-assignment-venue-helsinki&cart_value=1000&user_lat=60.17094")
@@ -15,7 +15,7 @@ async def test_endpoint_returns_correct_error_with_missing_query_user_lon_param(
 
 @pytest.mark.anyio
 async def test_endpoint_returns_correct_error_with_all_missing_query_parameters(
-        test_client):
+        test_client, mock_delivery_route_dependencies):
     """Tests that the API returns status code 422 with an error indicating that all parameters are missing"""
     response = test_client.get("/api/v1/delivery-order-price")
     assert response.status_code == 422
@@ -36,7 +36,7 @@ async def test_endpoint_returns_correct_error_with_unacceptable_venue_param(
 
 
 @pytest.mark.anyio
-async def test_return_empty_cart_error(test_client):
+async def test_return_empty_cart_error(test_client, mock_delivery_route_dependencies):
     """Tests that the API returns 400 and an error message for empty cart"""
     response = test_client.get(
         "/api/v1/delivery-order-price?venue_slug=home-assignment-venue-helsinki&cart_value=0&user_lat=60.17094&user_lon=24.93087")
